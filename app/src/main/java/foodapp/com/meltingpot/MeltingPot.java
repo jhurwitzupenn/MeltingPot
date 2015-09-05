49013b4666dfefda8b689b603cfcf061d326de43
martingreenberg <grmartin@seas.upenn.edu>
2015-09-05 14:38:19 -0400

    removed unnecessary libs, fixed android manifest

--- app/src/main/java/foodapp/com/meltingpot/MeltingPot.java
+++ app/src/main/java/foodapp/com/meltingpot/MeltingPot.java
@@ -2,14 +2,35 @@ package foodapp.com.meltingpot;
 
 import android.app.Application;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * Created by martingreenberg on 9/5/15.
  */
 public class MeltingPot extends Application {
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "RObjTBsw8FcRXFWSnwkS4T7quaNB95q3nzAqHtCL", "LHZacqbGSa22fiNaEcRyswaFq1ZjGM6smPyAD7AN");
+        ParseUser user = new ParseUser();
+        user.setUsername("my name");
+        user.setPassword("my pass");
+        user.setEmail("email@example.com");
+
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-555-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
     };
 }
