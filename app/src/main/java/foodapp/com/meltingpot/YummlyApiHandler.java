2069b510900b939706d0e397d5473e2710b082f6
jamespark04 <jampa@seas.upenn.edu>
2015-09-06 03:33:52 -0400

    slightly modified handler for urls

--- app/src/main/java/foodapp/com/meltingpot/YummlyApiHandler.java
+++ app/src/main/java/foodapp/com/meltingpot/YummlyApiHandler.java
@@ -7,6 +7,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -17,6 +18,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Formats yummly HttpRequests
@@ -44,17 +48,15 @@ public class YummlyApiHandler {
 
     public static void makeYummlyRequest(List<String> ingredients, final Activity activity) {
         String url = null;
-        try {
-            url = configureUrl(ingredients);
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalArgumentException("Unable to encode in UTF-8 format", e);
-        }
+        url = configureUrl(ingredients);
+
         Log.d("MELTING", url);
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         json = response;
+                        has_requested = true;
                         Log.d("MELTING", json.toString());
                     }
                 }, new Response.ErrorListener() {
@@ -65,10 +67,29 @@ public class YummlyApiHandler {
                     }
                 });
         Volley.newRequestQueue(activity).add(jsObjRequest);
-        has_requested = true;
+        // timeout after 10 seconds, blocks other threads
+        /**
+        String url = configureUrl(ingredients);
+        Log.d("MELTING", url);
+        RequestFuture<JSONObject> future = RequestFuture.newFuture();
+        JsonObjectRequest request = new JsonObjectRequest(url, null, future, future);
+        Volley.newRequestQueue(activity).add(request);
+        try {
+            json = future.get(10, TimeUnit.SECONDS);
+            has_requested = true;
+            Log.d("MELTING", "success");
+        } catch (InterruptedException e) {
+            // exception handling
+        } catch (ExecutionException e) {
+            // exception handling
+        } catch(TimeoutException e) {
+            Log.d("MELTING", "timeout");
+
+        }
+         */
     }
 
-    private static String configureUrl(List<String> ingredients) throws UnsupportedEncodingException {
+    private static String configureUrl(List<String> ingredients) {
         StringBuilder sb = new StringBuilder(URL_BASE + APP_ID + AMPERSAND + APP_KEY + AMPERSAND +
         REQUIRE_PICS + AMPERSAND);
         for (int i = 0; i < ingredients.size(); i ++) {
@@ -118,11 +139,11 @@ public class YummlyApiHandler {
         return l;
     }
 
-    public static List<List<String>> getImageUrls() {
-        List<List<String>> l = new ArrayList<>();
+    public static List<String> getRecipeUrls() {
+        List<String> l = new ArrayList<>();
         for (int i = 0; i < results.length(); i++) {
             try {
-                l.add(convertJSONArrayToList(results.getJSONObject(i).getJSONArray("smallImageUrls")));
+                l.add(results.getJSONObject(i).getString("sourceDisplayName"));
             }
             catch(JSONException e) {
                 Log.d("MELTING", "JSON Parsing Error" + e.getLocalizedMessage());
